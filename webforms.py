fdb60fc04acadfd56c720b722fc7ecb1221458b0
Sagar <chaudharysagar567@gmail.com>
2025-01-17 08:25:58 +0545

    email validation added

--- webforms.py
+++ webforms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField,PasswordField,BooleanField,ValidationError
-from wtforms.validators import DataRequired,EqualTo,Length
+from wtforms.validators import DataRequired,EqualTo,Length,Email
 
 # Login form
 class LoginForm(FlaskForm):
